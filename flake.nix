94180ae63144f0dffe8055a20c7498ac0f573366
marvin <marvin@thismarvin.com>
2021-10-17 19:41:25 -0500

    chore: nix flake update + formatting

--- flake.nix
+++ flake.nix
@@ -1,40 +1,40 @@
 {
-  description = "NixOS configuration";
+	description = "NixOS configuration";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    hm.url = "github:nix-community/home-manager";
-    neovim.url = "github:nix-community/neovim-nightly-overlay";
-  };
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		hm.url = "github:nix-community/home-manager";
+		neovim.url = "github:nix-community/neovim-nightly-overlay";
+	};
 
-  outputs = { self, nixpkgs, hm, ... }@inputs:
-  let
-    lib = nixpkgs.lib;
-    overlays = [ inputs.neovim.overlay ];
+	outputs = { self, nixpkgs, hm, ... }@inputs:
+	let
+		lib = nixpkgs.lib;
+		overlays = [ inputs.neovim.overlay ];
 
-  in {
-    nixosConfigurations = {
-      thinkpad = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
-      };
-    };
+	in {
+		nixosConfigurations = {
+			thinkpad = lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./configuration.nix
+				];
+			};
+		};
 
-    hmConfigurations = {
-      marvin = hm.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
-        username = "marvin";
-        homeDirectory = "/home/marvin";
-        configuration = {
-          nixpkgs.config.allowUnfree = true;
-          nixpkgs.overlays = overlays;
-          imports = [ ./home.nix ];
-        };
-      };
-    };
-  };
+		hmConfigurations = {
+			marvin = hm.lib.homeManagerConfiguration {
+				system = "x86_64-linux";
+				username = "marvin";
+				homeDirectory = "/home/marvin";
+				configuration = {
+					nixpkgs.config.allowUnfree = true;
+					nixpkgs.overlays = overlays;
+					imports = [ ./home.nix ];
+				};
+			};
+		};
+	};
 }
 
 # {
