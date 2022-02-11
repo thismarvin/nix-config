b8a061e3bc80ca3013eaa9a5f8fc6f5926764f81
marvin <me@thismarvin.com>
2022-02-10 19:44:17 -0600

    feat: isolated lua config in order to support init.vim

--- dotfiles/stable.lua
+++ dotfiles/stable.lua
@@ -1,18 +1,13 @@
-local keymap = vim.api.nvim_set_keymap
+-- Setup some utility functions.
+local function set(expression)
+	vim.api.nvim_command(string.format("set " .. expression))
+end
 
 local function execute(expression)
 	vim.api.nvim_exec(expression, true)
 end
 
-local function has(key)
-	return vim.fn.has(key) == 1
-end
-
-local function set(expression)
-	vim.api.nvim_command(string.format("set " .. expression))
-end
-
--- `vim.keymap.set` polyfill
+-- Implement a crude `vim.keymap.set` polyfill.
 if vim.keymap == nil then
 	vim.keymap = {
 		set = function(mode, lhs, rhs, options)
@@ -22,6 +17,10 @@ if vim.keymap == nil then
 
 			if options.noremap == nil then
 				options.noremap = true
+			end
+
+			if options.remap ~= nil and options.remap == true then
+				options.noremap = false
 			end
 
 			if type(mode) == "table" then
@@ -35,11 +34,9 @@ if vim.keymap == nil then
 	}
 end
 
-set("number")
-set("nowrap")
+set("relativenumber")
 set("noshowmode")
 set("confirm")
-set("autoread")
 set("noswapfile")
 set("undofile")
 set("smartindent")
@@ -49,10 +46,10 @@ set("splitbelow")
 set("splitright")
 set("list")
 set("listchars=tab:┊\\ ,trail:·")
--- set("clipboard=unnamedplus")
 set("mouse=a")
 set("spelllang=en_us")
 set("tabstop=2")
+set("softtabstop=2")
 set("shiftwidth=2")
 set("scrolloff=4")
 
@@ -60,8 +57,8 @@ execute([[
 augroup vimrc
   autocmd!
   " Only show the cursor line when in insert mode.
-  autocmd InsertEnter * set cursorline
-  autocmd InsertLeave * set nocursorline
+  " autocmd InsertEnter * set cursorline
+  " autocmd InsertLeave * set nocursorline
   " After yanking a selection, highlight the selection for a bit.
   autocmd TextYankPost * silent! lua vim.highlight.on_yank { higroup='Visual', timeout=200 }
 augroup end
@@ -70,9 +67,7 @@ augroup end
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<Leader>", "<Nop>")
-vim.keymap.set({ "n", "v" }, ";", ":")
 vim.keymap.set("n", "U", "<C-r>")
-vim.keymap.set("n", "<Leader>w", "<C-w>")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("n", "<C-Down>", "<Cmd>move .+1<CR>==zz")
@@ -87,3 +82,47 @@ vim.keymap.set("n", "N", "Nzz")
 vim.keymap.set("n", "<C-i>", "<C-i>zz")
 vim.keymap.set("n", "<C-o>", "<C-o>zz")
 vim.keymap.set("n", "<F12>", "<Cmd>setlocal spell!<CR>")
+
+-- netrw configuration.
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+
+-- nvim-treesitter configuration.
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "maintained",
+	ignore_install = { "norg" },
+	highlight = {
+		enable = true,
+	},
+})
+
+-- comment-nvim configuration.
+require("Comment").setup()
+
+-- telescope.nvim configuration.
+require("telescope").setup({
+	defaults = {
+		dynamic_preview_title = true,
+		path_display = { "smart" },
+		layout_strategy = "vertical",
+		layout_config = {
+			vertical = {
+				width = 0.8,
+				height = 0.9,
+			},
+		},
+	},
+	extensions = {
+		fzy_native = {
+			override_generic_sorter = false,
+			override_file_sorter = true,
+		},
+	},
+})
+
+-- nvim-lspconfig configuration.
+set("nobackup")
+set("nowritebackup")
+set("updatetime=250")
+set("shortmess+=c")
+set("signcolumn=yes")
