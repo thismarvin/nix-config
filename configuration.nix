66f86e047751220dd3a410c6397ee10e17670bec
marvin <me@thismarvin.com>
2022-03-16 10:19:55 -0500

    chore: dumped current nixos config

--- configuration.nix
+++ configuration.nix
@@ -1,84 +1,104 @@
+# TODO(thimarvin): Switch to zsh and automatically launch nushell?
+
 { config, pkgs, ... }:
 
+let
+	corePackages = with pkgs; [
+		curl
+		gcc
+		neovim
+	];
+	essentialPackages = with pkgs; [
+		bat
+		bottom
+		du-dust
+		fd
+		fzy
+		git
+		gitui
+		ripgrep
+		sd
+	];
+in
+
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+	imports = [
+		./hardware-configuration.nix
+	];
 
-  nix = {
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
+	nix = {
+		package = pkgs.nixUnstable;
+		extraOptions = ''
+			experimental-features = nix-command flakes
+		'';
+	};
 
-  nixpkgs = {
-    config = {
-      # allowUnfree = true;
-    };
-  };
+	nixpkgs.config.allowUnfree = true;
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	# Use the systemd-boot EFI boot loader.
+	boot.loader = {
+		systemd-boot.enable = true;
+		efi.canTouchEfiVariables = true;
+	};
 
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+	time.timeZone = "America/Chicago";
 
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
+	networking = {
+		hostName = "nixos";
+		wireless.enable = true;
+	};
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp2s0.useDHCP = true;
+	# The global useDHCP flag is deprecated, therefore explicitly set to false here.
+	# Per-interface useDHCP will be mandatory in the future, so this generated config
+	# replicates the default behaviour.
+	networking.useDHCP = false;
+	networking.interfaces.enp2s0.useDHCP = true;
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+	# Enable the X11 windowing system.
+	services.xserver = {
+		enable = true;
+		videoDrivers = [ "nvidia" ];
+		desktopManager.xfce.enable = true;
+	};
 
-  # Enable XFCE
-  services.xserver.desktopManager.xfce.enable = true;
-  programs.nm-applet.enable = true;
+	# Enable Sound
+	services.pipewire = {
+		enable = true;
+		alsa = {
+			enable = true;
+		};
+		pulse = {
+			enable = true;
+		};
+		jack = {
+			enable = true;
+		};
+	};
 
-  # Configure keymap in X11
-  services.xserver.layout = "us";
+	users.users.marvin = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" ];
+		shell = pkgs.nushell;
+	};
 
-  # Enable Sound
-  services.pipewire = {
-    enable = true;
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
-    pulse = {
-      enable = true;
-    };
-    jack = {
-      enable = true;
-    };
-  };
+	# List packages installed in system profile.
+	environment.systemPackages = corePackages ++ essentialPackages;
 
-  users.users.marvin = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    shell = pkgs.nushell;
-  };
+	hardware = {
+		opengl = {
+			enable = true;
+			driSupport = true;
+			driSupport32Bit = true;
+		};
+		steam-hardware.enable = true;
+	};
 
-  # List packages installed in system profile.
-  environment.systemPackages = with pkgs; [
-    curl
-    neovim
-  ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	system.stateVersion = "21.11"; # Did you read the comment?
 }
--- home.nix
+++ home.nix
@@ -1,157 +1,145 @@
 { config, pkgs, ... }:
 
 {
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  # Home Manager needs a bit of information about you and the paths it should manage.
-  home.username = "marvin";
-  home.homeDirectory = "/home/marvin";
-
-  # Packages that should be installed to the user profile.
-  home.packages = with pkgs; [
-    # neovim
-    # nxengine-evo # Unfree
-    bat
-    bitwarden
-    bottom
-    dolphin-emu-beta
-    du-dust
-    element-desktop
-    emacs
-    exa
-    fd
-    firefox
-    fzy
-    genact
-    gh
-    git
-    gitui
-    gnome.gnome-chess
-    helix
-    inkscape
-    jetbrains-mono
-    jitsi-meet-electron
-    joplin-desktop
-    kitty
-    logseq
-    mpv
-    mupen64plus
-    mypaint
-    nnn
-    nushell
-    pavucontrol
-    ripgrep
-    sameboy
-    sd
-    stockfish
-    thunderbird
-    ungoogled-chromium
-    zoxide
-  ];
-
-  programs = {
-    # firefox = {
-    #   enable = true;  
-    #   profiles = {
-    #     default = {
-    #       userChrome = ''
-    #         #TabsToolbar {
-    #           visibility: collapse !important;
-    #         }
-    #       '';
-    #     };
-    #   };
-    # };
-    kitty = {
-      enable = true;
-      environment = {
-        "EDITOR" = "${pkgs.neovim}/bin/nvim";
-        "MANPAGER" = "${pkgs.neovim}/bin/nvim +Man!";
-      };
-      settings = {
-        font_size = "10";
-        font_family = "JetBrainsMono-Bold";
-        background_opacity = "0.9";
-        window_padding_width = "8";
-        tab_bar_style = "powerline";
-        enabled_layouts = "vertical,horizontal";
-	# shell_integration = "disabled";
-      };
-      keybindings = {
-        "kitty_mod+t" = "launch --cwd=current --type=tab";
-        "kitty_mod+n" = "launch --cwd=current";
-        "kitty_mod+enter" = "new_os_window";
-      };
-      extraConfig = ''
-        include current-theme.conf
-      '';
-    };
-    nushell = {
-      enable = true;
-      settings = {
-        skip_welcome_message = true;
-        startup = [ 
-          "${pkgs.zoxide}/bin/zoxide init nushell --hook prompt | save ~/.zoxide.nu"
-          "source ~/.zoxide.nu"
-          "alias v = ${pkgs.neovim}/bin/nvim"
-          "alias ll = ${pkgs.exa}/bin/exa -l"
-          "alias lla = ${pkgs.exa}/bin/exa -la"
-          "alias n = ${pkgs.nnn}/bin/nnn -deo"
-          "alias d = ${pkgs.kitty}/bin/kitty +kitten diff"
-      	];
-      };
-    };
-    git = {
-      enable = true;    
-      userName = "marvin";
-      userEmail = "me@thismarvin.com";
-      extraConfig = {
-        init = {
-	  defaultBranch = "dev";
+	home = {
+		username = "marvin";
+		homeDirectory = "/home/marvin";
+		stateVersion = "21.11";
+		packages = with pkgs; [
+			bitwarden
+			dolphin-emu-beta
+			element-desktop
+			emacs
+			exa
+			fira-code
+			firefox
+			genact
+			gh
+			gnome.gnome-chess
+			helix
+			inkscape
+			jetbrains-mono
+			jitsi-meet-electron
+			joplin-desktop
+			kitty
+			logseq
+			mpv
+			mupen64plus
+			mypaint
+			nnn
+			nushell
+			nxengine-evo
+			pavucontrol
+			polymc
+			sameboy
+			steamPackages.steam
+			steamPackages.steam-runtime
+			stockfish
+			thunderbird
+			ungoogled-chromium
+			zoxide
+			zsh
+		];
 	};
-      };
-    };
-    neovim = {
-      enable = true;
-      plugins = with pkgs.vimPlugins; [
-	comment-nvim
-	editorconfig-nvim
-	lsp_extensions-nvim
-	nvim-lspconfig
-	nvim-treesitter
-	telescope-nvim
-      ];
-      extraConfig = ''
-        lua require("stable")
-      '';
-    };
-    helix = {
-      enable = true;
-      settings = {
-        theme = "dark_plus";
-        editor = {
-          line-number = "relative";
-          # cursor-shape = {
-          #   normal = "block";    
-          #   insert = "bar";    
-          #   select = "underline";    
-          # };
-        };
-        keys = {
-          normal = {
-          };
-        };
-      };   
-    };
-  };
-  xdg.configFile."nvim/lua/stable.lua".source = ./dotfiles/stable.lua;
 
-  # This value determines the Home Manager release that your configuration is compatible with.
-  # This helps avoid breakage when a new Home Manager release introduces backwards incompatible
-  # changes.
-  #
-  # You can update Home Manager without changing this value. See the Home Manager release notes
-  # for a list of state version changes in each release.
-  home.stateVersion = "21.11";
+	programs = {
+		home-manager = {
+			enable = true;
+		};
+		kitty = {
+			enable = true;
+			environment = {
+				"EDITOR" = "${pkgs.neovim}/bin/nvim";
+				"MANPAGER" = "${pkgs.neovim}/bin/nvim +Man!";
+			};
+			settings = {
+				font_size = "10";
+				font_family = "FiraCodeRoman_700wght";
+				font_features = "FiraCodeRoman_700wght +zero +ss02 +ss03 +ss04 +ss07";
+				bold_font = "JetBrainsMono-ExtraBold";
+				italic_font = "JetBrainsMono-Italic";
+				bold_italic_font = "JetBrainsMono-ExtraBoldItalic";
+				background_opacity = "0.9";
+				window_padding_width = "8";
+				tab_bar_style = "powerline";
+				enabled_layouts = "vertical,horizontal";
+			};
+			keybindings = {
+				"kitty_mod+t" = "launch --cwd=current --type=tab";
+				"kitty_mod+n" = "launch --cwd=current";
+				"kitty_mod+enter" = "new_os_window";
+			};
+			extraConfig = ''
+				symbol_map U+0030-U+0039 JetBrainsMono-Bold
+				symbol_map U+0041-U+005A JetBrainsMono-Bold
+				symbol_map U+0061-U+007A JetBrainsMono-Bold
+				include current-theme.conf
+			'';
+		};
+		nushell = {
+			enable = true;
+			settings = {
+				skip_welcome_message = true;
+				startup = [
+					"${pkgs.zoxide}/bin/zoxide init nushell --hook prompt | save ~/.zoxide.nu"
+					"source ~/.zoxide.nu"
+					"alias v = ${pkgs.neovim}/bin/nvim"
+					"alias ll = ${pkgs.exa}/bin/exa -l"
+					"alias lla = ${pkgs.exa}/bin/exa -la"
+					"alias n = ${pkgs.nnn}/bin/nnn -deo"
+					"alias d = ${pkgs.kitty}/bin/kitty +kitten diff"
+				];
+			};
+		};
+		git = {
+			enable = true;
+			userName = "marvin";
+			userEmail = "me@thismarvin.com";
+			extraConfig = {
+				init = {
+					defaultBranch = "dev";
+				};
+			};
+		};
+		zoxide = {
+			enable = true;
+			enableBashIntegration = false;
+			enableZshIntegration = false;
+			enableFishIntegration = false;
+		};
+		neovim = {
+			enable = true;
+			plugins = with pkgs.vimPlugins; [
+				comment-nvim
+				editorconfig-nvim
+				lsp_extensions-nvim
+				nvim-lspconfig
+				nvim-treesitter
+				telescope-nvim
+			];
+			extraConfig = ''
+				lua require("stable")
+			'';
+		};
+		helix = {
+			enable = true;
+			settings = {
+				theme = "dark_plus";
+				editor = {
+					line-number = "relative";
+					# cursor-shape = {
+					# 	normal = "block";
+					# 	insert = "bar";
+					# 	select = "underline";
+					# };
+				};
+				keys = {
+					normal = {
+					};
+				};
+			};
+		};
+	};
+
+	xdg.configFile."nvim/lua/stable.lua".source = ./dotfiles/stable.lua;
 }
